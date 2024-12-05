c0f52803e58d98caebb36d59615f0dfa4ae65824
Acedia Zhang <Acediazhang@gmail.com>
2024-12-05 15:14:34 -0600

    refactor: ringpuzzle

--- RingPuzzle.py
+++ RingPuzzle.py
@@ -1,25 +1,27 @@
 import random
 
+
 class NurikabeSolver():
     def __init__(self, board):
         self.board = board
         self.size_row = len(board)
         self.size_col = len(board[0])
-        self.islands = {((row, col), board[row][col]): [(row, col)] for row in range(self.size_row) for col in range(self.size_col) if board[row][col] != 0}
+        self.islands = {((row, col), board[row][col]): [(row, col)] for row in range(
+            self.size_row) for col in range(self.size_col) if board[row][col] != 0}
         self.clues = {}
         self.solution = []
         for r in range(self.size_row):
             for c in range(self.size_col):
                 if self.board[r][c] != -1:
-                    self.clues[(r,c)] = self.board[r][c]
+                    self.clues[(r, c)] = self.board[r][c]
                     self.board[r][c] = 1
 
     def check_solution(self, completed=True):
         if completed:
             if not self.check_complete():
                 return False
-            for (r,c), size in self.clues.items():
-                if self.check_island_size(r,c) != size:
+            for (r, c), size in self.clues.items():
+                if self.check_island_size(r, c) != size:
                     return False
             if not self.check_no_more_island():
                 return False
@@ -27,15 +29,15 @@ class NurikabeSolver():
                 return False
             if not self.check_2x2(self.board):
                 return False
-            return True          
+            return True
         else:
-            for (r,c), size in self.clues.items():
-                if self.check_island_size(r,c) > size:
+            for (r, c), size in self.clues.items():
+                if self.check_island_size(r, c) > size:
                     return False
             if not self.check_2x2(self.board):
                 return False
             return True
-    
+
     def check_2x2(self, board):
         for i in range(self.size_row - 1):
             for j in range(self.size_col - 1):
@@ -43,12 +45,12 @@ class NurikabeSolver():
                     if board[i][(j + 1) % self.size_col] == board[i + 1][(j + 1) % self.size_col] == 0:
                         return False
         return True
-    
+
     def check_no_more_island(self):
         island_in_clues = 0
         for size in self.clues.values():
             island_in_clues += size
-        
+
         island_in_board = 0
         for r in range(self.size_row):
             for c in range(self.size_col):
@@ -65,31 +67,30 @@ class NurikabeSolver():
                 if self.board[r][c] == -1:
                     return False
         return True
-    
+
     def dfs(self, row, col, target, visited, state):
         state[row][col] = 1
-        if (row,col) in visited:
+        if (row, col) in visited:
             return 0
         if not (0 <= row < self.size_row):
             return 0
         if self.board[row][col] != target:
             return 0
-        
-        visited.add((row,col))
+
+        visited.add((row, col))
         size = 1
-        for dr, dc in [(-1,0),(1,0),(0,-1),(0,1)]:
+        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             new_row = row + dr
             new_col = (col + dc) % self.size_col
             if (0 <= new_row < self.size_row):
                 if state[new_row][new_col] == 0:
-                    size += self.dfs(new_row, new_col, target, visited,state)
+                    size += self.dfs(new_row, new_col, target, visited, state)
         return size
 
-
-    def check_island_size(self,row,col):
+    def check_island_size(self, row, col):
         visited = set()
         state = [[0 for _ in range(self.size_col)] for _ in range(self.size_row)]
-        return self.dfs(row, col, 1,visited,state)
+        return self.dfs(row, col, 1, visited, state)
 
     def check_ocean_continuous(self):
         visited = set()
@@ -98,18 +99,18 @@ class NurikabeSolver():
         for r in range(self.size_row):
             for c in range(self.size_col):
                 if self.board[r][c] == 0:
-                    start = (r,c)
+                    start = (r, c)
                     break
             if start:
                 break
         if not start:
             return False
-        
-        self.dfs(start[0],start[1], 0, visited, state)
+
+        self.dfs(start[0], start[1], 0, visited, state)
 
         for r in range(self.size_row):
             for c in range(self.size_col):
-                if self.board[r][c] == 0 and (r,c) not in visited:
+                if self.board[r][c] == 0 and (r, c) not in visited:
                     return False
         return True
 
@@ -126,7 +127,7 @@ class NurikabeSolver():
         if self.board[row][col] != -1:
             return self.trackback(row, col + 1)
 
-        for value in [0, 1]: 
+        for value in [0, 1]:
             self.board[row][col] = value
             if self.check_solution(completed=False):
                 self.trackback(row, col + 1)
@@ -141,13 +142,12 @@ class NurikabeSolver():
 
     def print_solution(self):
         for i in range(len(self.solution)):
-            print("Solution",i+1, ":")
+            print("Solution", i + 1, ":")
             for row in self.solution[i]:
                 print("".join(['#' if x == 1 else '.' if x == 0 else '-' for x in row]))
-        
 
     def generate_puzzle(self, row, col):
-        while True:    
+        while True:
             solution = [[random.choice([0, 1]) for _ in range(col)] for _ in range(row)]
             self.board = solution
             self.size_row = row
@@ -165,27 +165,25 @@ class NurikabeSolver():
                 for c in range(col):
                     if solution[r][c] == 1 and state[r][c] == 0:
                         visited = set()
-                        island_size = self.dfs(r,c,1,visited,state)
-                        self.clues[(r,c)] = island_size
+                        island_size = self.dfs(r, c, 1, visited, state)
+                        self.clues[(r, c)] = island_size
                         # self.clues[random.choice(list(visited))] = island_size
                         continue
             for r in range(row):
                 for c in range(col):
-                    if (r,c) in self.clues:
+                    if (r, c) in self.clues:
                         self.board[r][c] = 1
                     else:
                         self.board[r][c] = -1
-            
+
             self.solve()
             if len(self.solution) != 1:
                 continue
             else:
                 puzzle = [[-1 for _ in range(col)] for _ in range(row)]
-                for (r,c), size in self.clues.items():
+                for (r, c), size in self.clues.items():
                     puzzle[r][c] = size
                 return puzzle
-
-
 
 
 # Example puzzle input
@@ -204,7 +202,7 @@ puzzle = [
 # Solve the Nurikabe puzzle
 solver = NurikabeSolver(puzzle)
 
-puzzle_new = solver.generate_puzzle(3,3)
+puzzle_new = solver.generate_puzzle(3, 3)
 for row in puzzle_new:
     print(row)
 
@@ -214,5 +212,3 @@ if solver.solve():
     solver.print_solution()
 else:
     print("No solution exists.")
-    
-    
